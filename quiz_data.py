17e536fca922c6b682e6f4ffb3716abacd412521
Kanikasahu12 <161454584+Kanikasahu12@users.noreply.github.com>
2024-05-17 01:18:36 +0530

    Python_Project

---
+++ quiz_data.py
@@ -0,0 +1,53 @@
+quiz_data = [
+    {
+        "question": "What animal is the national symbol of Australia?",
+        "choices": ["Kangaroo", "Koala", "Lion", "Cheetah"],
+        "answer": "Kangaroo"
+    },
+    {
+        "question": "What chemical element is designated as “Cu”?",
+        "choices": ["Zinc", "Copper", "Silver", "Mercury"],
+        "answer": "Copper"
+    },
+    {
+        "question": "Who was the first person to walk on the moon?",
+        "choices": ["Yuri Gagarin", "Neil Armstrong", "John Glenn", "Buzz Aldrin"],
+        "answer": "Neil Armstrong"
+    },
+    {
+        "question": "Which country is known as the 'Land of the Rising Sun?",
+        "choices": ["China", "Japan", "South Korea", "Thailand"],
+        "answer": "Japan"
+    },
+     {
+        "question": "Which river is the longest in the world?",
+        "choices": ["Amazon", "Mississippi", "Nile", "Yangtze"],
+        "answer": "Nile"
+    },
+      {
+        "question": "Which planet in the solar system is known as the “Red Planet?",
+        "choices": ["Venus", "Earth", "Mars", "Jupiter"],
+        "answer": "Mars"
+    },
+       {
+        "question": "Which is the largest island?",
+        "choices": ["New Guinea", "Andaman Nicobar", "Greenland", "Hawaii"],
+        "answer": "Greenland"
+    },
+        {
+        "question": "Which city is the capital of Italy?",
+        "choices": ["Venice", "Florence", "Rome", "Milan"],
+        "answer": "Rome"
+    },
+         {
+        "question": "Which of the following inventions is attributed to Thomas Edison?",
+        "choices": ["Bulb", "Telephone", "Computer", "Rocket"],
+        "answer": "Bulb"
+    },
+          {
+        "question": "Who wrote the play “Romeo and Juliet?",
+        "choices": ["William Shakespeare", "Alexander Pushkin", "Fyodor Dostoyevsky", "Victor Hugo"],
+        "answer": "William Shakespeare"
+    }
+    # Add more questions here
+]
